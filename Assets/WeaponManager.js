df4a0fb77ef2a0c14674c1d27efb964ba04cbe1e
cmf029 <cmf029@users.noreply.github.com>
2014-04-21 18:57:11 -0500

    Add a weapon inventory system
    Use the keys 1-5 to switch weapons

--- Assets/WeaponManager.js
+++ Assets/WeaponManager.js
@@ -1,24 +1,73 @@
 ﻿#pragma strict
-
-public var cweapon : GameObject = null;
+@HideInInspector
+public var weapons : GameObject[];
+@HideInInspector
+public var cweaponIndex = 0;
 
 function Start()
 {
-	if ( cweapon != null )
+	weapons = new GameObject[5];
+	for( var i = 0; i < weapons.Length; i++ )
 	{
-		SetWeapon( cweapon );
+		weapons[ i ] = null;
 	}
 }
 
+function DisableWeapons()
+{
+	for ( var  i = 0; i < weapons.Length; i++ )
+	{
+		if ( weapons[ i ] != null )
+		{
+			weapons[ i ].SetActive( false );
+		}
+	}
+}
+
+function Update()
+{
+	// check for inventory switches
+	for ( var i : int = 1; i <= 5; i++ )
+	{
+		if ( Input.GetKey( i.ToString() ) )
+		{
+			if ( weapons[ i - 1 ] != null )
+			{
+				DisableWeapons();
+				weapons[ i - 1 ].SetActive( true );
+				cweaponIndex = i - 1;
+				break;
+			}
+		}
+	}
+}
 function SetWeapon( inweapon : GameObject )
 {
 	var weapon : GameObject = Instantiate(inweapon);
 	weapon.transform.parent = transform;
 	weapon.transform.localPosition = weapon.transform.position;
 	weapon.SetActive(true);
-	if ( cweapon != null )
+	var i : int;
+	
+	// find a free slot
+	for ( i = 0; i < weapons.Length; i++ )
 	{
-		Destroy(cweapon);
+		if ( weapons[ i ] == null )
+		{
+			weapons[ i ] = weapon;
+			cweaponIndex = i;
+			break;
+		}
 	}
-	cweapon = weapon;
+	
+	// coult not find a free slot, so replace the existing weapon
+	if ( i == weapons.Length )
+	{
+		if ( weapons[cweaponIndex] != null )
+		{
+			Destroy(weapons[cweaponIndex]);
+			weapons[cweaponIndex] = null;
+		}
+		weapons[cweaponIndex] = weapon;
+	}
 }
