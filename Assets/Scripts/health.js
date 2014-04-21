c409e237b61ad26b007d48186e34668bd6c2301d
cmf029 <cmf029@users.noreply.github.com>
2014-04-21 14:58:07 -0500

    Add enemy spawn zones
    These will control the spawning of enemies

--- Assets/Scripts/health.js
+++ Assets/Scripts/health.js
@@ -4,11 +4,14 @@ public var spawnPoint : Transform;
 
 @HideInInspector
 public var maxHealth : int;
+@HideInInspector
+public var numDeaths : int;
 
 function Start()
 {
 	maxHealth = health;
 	transform.position = spawnPoint.position; // spawn at the correct point
+	numDeaths = 0;
 }
 
 function Update()
@@ -17,5 +20,6 @@ function Update()
 	{
 		transform.position = spawnPoint.position;
 		health = maxHealth;
+		numDeaths++;
 	}
 }
---
+++ Assets/enemyspawnzone.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var enemy : GameObject;
+var numEnemies : int;
+var maxDeaths : int;
+
+private var enemies : ArrayList;
+
+function Start () {
+	var children = GetComponentsInChildren(Transform);
+	enemies = new ArrayList();
+	for ( var  i = 0; i < numEnemies; i++ )
+	{
+		var enemy : GameObject = Instantiate(enemy);
+		enemy.SetActive( true );
+		enemy.GetComponent(health).spawnPoint = children[i%children.length].transform;
+		enemies.Add( enemy );
+	}
+}
+
+function Update () {
+	var numDeaths = 0;
+	
+	for ( var i = 0; i < enemies.Count; i++ )
+	{
+		numDeaths += ( enemies[ i ] as GameObject ).GetComponent(health).numDeaths;
+	}
+	
+	if ( numDeaths >= maxDeaths )
+	{
+		for ( i = 0; i < enemies.Count; i++ )
+		{
+			Destroy(enemies[i] as GameObject);
+		}
+		enemies = new ArrayList();
+	}
+}
