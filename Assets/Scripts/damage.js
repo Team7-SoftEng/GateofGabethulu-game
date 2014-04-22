e66921271c47357c84ccf0bd834b9bb1b0d1aec4
cmf029 <cmf029@users.noreply.github.com>
2014-04-21 20:46:06 -0500

    Add a repeat time to damages
    This prevents issues with damage rates based on the arbitrary trigger
    callbacks.

--- Assets/Scripts/damage.js
+++ Assets/Scripts/damage.js
@@ -2,10 +2,22 @@
 
 public var amount = 5;
 public var damagetag : String;
+public var repeatTime : float = 1;
+private var lastDamageTime : float = 0;
+
+function damage(other: Collider2D)
+{
+	if ( other.tag == damagetag && lastDamageTime + repeatTime < Time.timeSinceLevelLoad ) {
+		other.GetComponent( health ).health -= amount;
+		lastDamageTime = Time.timeSinceLevelLoad;
+	}
+}
 
 function OnTriggerEnter2D(other: Collider2D) {
-	if ( other.tag == damagetag ) {
-		other.GetComponent( health ).health -= amount;
-	}
+	damage(other);
+}
+
+function OnTriggerStay2D(other: Collider2D) {
+	damage(other);
 }
 
