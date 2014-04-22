7715ab5051c4bae11a28c8bd09acbe265c0b7beb
cmf029 <cmf029@users.noreply.github.com>
2014-04-22 00:27:20 -0500

    Add a bow and arrow weapon

---
+++ Assets/Scripts/ShotMoveController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var moveSpeed : float;
+var lifeTime : float;
+
+@HideInInspector
+var dir : Vector3;
+private var creationTime = 0;
+
+function Update ()
+{
+	if ( creationTime == 0 )
+	{
+		creationTime = Time.timeSinceLevelLoad;
+		dir = Vector3.Normalize(dir);
+	}
+	
+	if ( creationTime + lifeTime < Time.timeSinceLevelLoad )
+	{
+		Destroy( this.gameObject );
+	}
+	else
+	{
+		transform.position += dir * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/rangedattack.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+public var shot : GameObject;
+public var fireRate : float;
+private var timeLastFired : float = 0;
+
+function Update () {
+	
+	var down = Input.GetButton( "Fire1" );
+	
+	if ( down )
+	{
+		transform.RotateAround(transform.parent.transform.position, Vector3.forward, 360 * Time.deltaTime );
+	}
+	
+	if ( timeLastFired + fireRate < Time.timeSinceLevelLoad )
+	{
+		var nshot : GameObject = Instantiate(shot);
+		nshot.SetActive( true );
+		nshot.transform.position = transform.position;
+		//nshot.transform.parent = transform;
+		var moveCont = nshot.GetComponent(ShotMoveController);
+		moveCont.dir = Vector3.Normalize( transform.position - transform.parent.transform.position );
+	
+		// rotate the shot so it is facing the correct direction
+		nshot.transform.Rotate( Quaternion.LookRotation(Vector3.forward,moveCont.dir).eulerAngles );
+		timeLastFired = Time.timeSinceLevelLoad;
+	}
+}
