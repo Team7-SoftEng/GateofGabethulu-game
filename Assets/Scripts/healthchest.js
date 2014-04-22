ddedfa03f7e0d980bd41333274592c73fa67a700
cmf029 <cmf029@users.noreply.github.com>
2014-04-21 23:25:07 -0500

    Add a health regenerating chest

---
+++ Assets/Scripts/healthchest.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var chestOpen : Sprite;
+var chestClosed : Sprite;
+var healthRegen : int;
+
+private var open : boolean = false;
+
+function Start () {
+	GetComponent(SpriteRenderer).sprite = chestClosed;
+}
+
+
+function OnTriggerEnter2D( other: Collider2D )
+{
+	if ( other.tag == "Player" && !open )
+	{
+		GetComponent(SpriteRenderer).sprite = chestOpen;
+		var hComp = other.GetComponent(health);
+		hComp.health += healthRegen;
+		open = true;
+	}
+}
