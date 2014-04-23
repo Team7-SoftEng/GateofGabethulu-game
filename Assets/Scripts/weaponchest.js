e0c496b95599ddbdb9c3381985d8526946cefade
cmf029 <cmf029@users.noreply.github.com>
2014-04-23 10:42:58 -0500

    Show the contents of opened chests

--- Assets/Scripts/weaponchest.js
+++ Assets/Scripts/weaponchest.js
@@ -5,6 +5,7 @@ var chestClosed : Sprite;
 var insideWeapon : GameObject;
 
 private var open : boolean = false;
+private var empty : boolean = false;
 
 function Start () {
 	GetComponent(SpriteRenderer).sprite = chestClosed;
@@ -16,8 +17,19 @@ function OnTriggerEnter2D( other: Collider2D )
 	if ( other.tag == "Player" && !open )
 	{
 		GetComponent(SpriteRenderer).sprite = chestOpen;
-		var man = other.GetComponent(WeaponManager);
-		man.SetWeapon(insideWeapon);
+		var child = transform.FindChild("chestcontent");
+		child.GetComponent(SpriteRenderer).sprite = insideWeapon.GetComponent(SpriteRenderer).sprite;
 		open = true;
+	}
+}
+
+function OnTriggerStay2D( other: Collider2D )
+{
+	if ( other.tag == "Player" && !empty && open && Input.GetKey( "e" ) )
+	{
+		Destroy(transform.FindChild("chestcontent").gameObject);
+		var man : WeaponManager = other.GetComponent(WeaponManager);
+		man.SetWeapon( insideWeapon );
+		empty = true;
 	}
 }
