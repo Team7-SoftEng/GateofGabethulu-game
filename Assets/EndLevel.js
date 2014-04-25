861a375299827a7133db294be1b5b8678fd2fe63
cmf029 <cmf029@github.com>
2014-04-25 16:51:48 -0500

    Add the beginning of a second level
    The second level auto starts after defeating the final boss in the
    tutorial level. It cannot currently be tested on its own because the
    player and camera are preserved from the tutorial level.

---
+++ Assets/EndLevel.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+public var level : int;
+
+function OnEnemiesDefeated()
+{
+	var pl = GameObject.FindGameObjectWithTag("Player");
+	DontDestroyOnLoad( pl );
+	DontDestroyOnLoad( Camera.main );
+	Application.LoadLevel( level );
+}
---
+++ Assets/startlevel.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var spawnPoint : Transform;
+
+function Awake() {
+	var player = GameObject.FindWithTag("Player");
+	player.transform.position = spawnPoint.position;
+}
