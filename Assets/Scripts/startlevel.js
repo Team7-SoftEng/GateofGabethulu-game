8c23e483a99a39052b04ba541940b30ef68a7f59
cmf029 <cmf029@github.com>
2014-04-25 21:20:27 -0500

    Allow level 1 to be started standalone
    There was previously not player or camera in the scene, so the only way
    to actually play it was to complete the tutorial level first.

---
+++ Assets/Scripts/startlevel.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var inactiveplayer : GameObject;
+var inactivecamera : GameObject;
+var spawnPoint : Transform;
+
+function Awake() {
+	var player = GameObject.FindWithTag("Player");
+	
+	if ( player == null )
+	{
+		// if there is no active player, then this level was started out of order, so activate this level's camera and player objects
+		inactiveplayer.SetActive( true );
+		inactivecamera.SetActive( true );
+	}
+	else
+	{
+		// reset the player's spawn point
+		player.transform.position = spawnPoint.position;
+		player.GetComponent(respawn).spawnPoint = spawnPoint;
+	}
+}
--- Assets/startlevel.js
+++
@@ -1,8 +0,0 @@
-﻿#pragma strict
-
-var spawnPoint : Transform;
-
-function Awake() {
-	var player = GameObject.FindWithTag("Player");
-	player.transform.position = spawnPoint.position;
-}
