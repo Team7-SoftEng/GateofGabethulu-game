14fba332502562cdfdd5874de98d2da23345cb96
Amm073 <Amm073@shsu.edu>
2014-04-27 18:04:25 -0500

    Tutorial pop up

---
+++ Assets/Scripts/movementTxt.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var Movementtxt : GameObject;
+
+function Start () {
+Movementtxt.active=true;}
+
+function Update () {
+if(Input.GetKey(KeyCode.Escape)){
+Movementtxt.active=false;}}
